08440859ef744e4bf65157ba102815caebcebf6a
CJonesCode <34059991+CJonesCode@users.noreply.github.com>
2025-06-21 16:56:27 -1000

    fix: supabase envs

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -2,9 +2,14 @@ import { cookies } from "next/headers"
 import { createServerClient } from "@supabase/ssr"
 
 export function createServerSupabase() {
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
-  )
+  const supabaseUrl =
+    process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL
+  const supabaseKey =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Supabase environment variables are not set")
+  }
+
+  return createServerClient(supabaseUrl, supabaseKey, { cookies })
 } 
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -7,4 +7,7 @@ import { createBrowserClient } from "@supabase/ssr"
 //   const supabase = createServerComponentClient({ cookies })
 // or const supabase = createRouteHandlerClient({ cookies })
 
-export const supabase = createBrowserClient()
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY!
+)
